ef25d1e3b6e30bf2a584dce1b3a6057fb085034a
galqawala <github.prayerlessly@aleeas.com>
2022-11-18 19:20:58 +0200

    hasEnoughEnergyForAnotherUpgrader() threshold 250

--- src/main.ts
+++ src/main.ts
@@ -640,7 +640,7 @@ function hasEnoughEnergyForAnotherUpgrader(controller: StructureController) {
     .filter(creep => creep.memory.upgrade === controller.id)
     .reduce((aggregated, item) => aggregated + item.getActiveBodyparts(WORK), 13 /* initial*/);
   const energyPerWork = energy / assignedWorkParts;
-  const isEnough = energyPerWork > 200;
+  const isEnough = energyPerWork > 250;
   return isEnough;
 }
 
