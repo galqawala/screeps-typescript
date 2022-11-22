586269d84f9a016f289caa4ade9f0a53f17bdbc1
galqawala <github.prayerlessly@aleeas.com>
2022-11-22 22:55:43 +0200

    More carriers to retrieve from storage

--- src/main.ts
+++ src/main.ts
@@ -1841,7 +1841,7 @@ function getCarrierEnergySource(creep: Creep) {
     );
     if (room.storage) {
       const carriersForStorage = Math.ceil(
-        (room.memory.stickyEnergy[room.storage.id] / STORAGE_CAPACITY) * 3
+        (room.memory.stickyEnergy[room.storage.id] / STORAGE_CAPACITY) * 4
       );
       if (countCarriersBySource(room.storage.id) < carriersForStorage) containers.push(room.storage);
     }
