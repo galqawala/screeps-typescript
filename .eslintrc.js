2658eebda90f62919a1e1da39df96c810fe9f476
galqawala <github.prayerlessly@aleeas.com>
2024-08-28 00:12:36 +0300

    "max-lines": ["warn", 1666],

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,7 +88,7 @@ module.exports = {
     "id-match": "error",
     "linebreak-style": "off",
     "max-classes-per-file": ["error", 1],
-    "max-lines": ["warn", 1664],
+    "max-lines": ["warn", 1666],
     "max-lines-per-function": ["warn", 39],
     "new-parens": "off",
     "newline-per-chained-call": "off",
